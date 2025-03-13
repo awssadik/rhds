ab4f623ddffbf19de55b3a92b49dd7cd6c529907
awssadik <aws.sadik@bristol.ac.uk>
2025-03-13 13:51:06 +0000

    quarto document

--- scripts/run-analysis.r
+++ scripts/run-analysis.r
@@ -2,6 +2,7 @@
 ########### RUN ALL #############################
 #################################################
 
+source("scripts/download-pan-cancer-clinical.r")
 source("scripts/01-extract-data.r")
 source("scripts/02-clean-clinical.r")
 source("scripts/03-predict-proteins.r")
